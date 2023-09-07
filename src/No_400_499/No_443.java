957722a6b0ac80f034c5e10364ad687ff7abf4b8
runhanshi <ShiRunhan0329@outlook.com>
2023-09-07 10:21:27 -0700

    09/06/2023

---
+++ src/No_400_499/No_443.java
@@ -0,0 +1,20 @@
+package No_400_499;
+// No.443 String Compression
+public class No_443 {
+    public int compress(char[] chars){
+        int k = 0;
+        int n = chars.length;
+        for (int i = 0, j = i + 1; i < n;){
+            while(j < n && chars[j] == chars[i]) j++;
+            chars[k++] = chars[i];
+            if (j - i > 1) {
+                String cnt = String.valueOf(j - i);
+                for (char c: cnt.toCharArray()){
+                    chars[k++] = c;
+                }
+            }
+            i = j;
+        }
+        return k;
+    }
+}
---
+++ src/No_400_499/No_445.java
@@ -0,0 +1,46 @@
+package No_400_499;
+
+import java.util.Stack;
+
+// No.445 Add Two Numbers II
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+public class No_445 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        Stack<Integer> stk1 = new Stack<>();
+        while(l1 != null) {
+            stk1.push(l1.val);
+            l1 = l1.next;
+        }
+        Stack<Integer> stk2 = new Stack<>();
+        while(l2 != null) {
+            stk2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        int carry = 0;
+        while (carry > 0 || !stk1.isEmpty() || !stk2.isEmpty()){
+            int val = carry;
+            if (!stk1.isEmpty()) val += stk1.pop();
+            if (!stk2.isEmpty()) val += stk2.pop();
+
+            carry = val / 10;
+            val = val % 10;
+            ListNode curr = new ListNode(val);
+            curr.next = dummy.next;
+            dummy.next = curr;
+        }
+        return dummy.next;
+    }
+}
